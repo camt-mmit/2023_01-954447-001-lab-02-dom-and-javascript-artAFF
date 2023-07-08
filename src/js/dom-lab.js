6302cd54a944c4a311fa6a35fc2f904a221a11c1
artAFF <art911191@gmail.com>
2023-07-09 02:22:06 +0700

    origin/main

---
+++ src/js/dom-lab.js
@@ -0,0 +1,5 @@
+import { createComponent } from "./input-component.js";
+
+createComponent(document.body);
+
+// npx gh-pages -d src
--- src/js/input-component.js
+++ src/js/input-component.js
@@ -1,81 +1,104 @@
 export function createComponent(componentElement) {
-    const tempInput = componentElement.querySelector("template.app-temp-input");
-    const inputList = tempInput.parentElement;
-  
-    const updateResult = () => {
-      const children = [...inputList.children].filter(
-        (elem) => elem !== tempInput
-      );
-  
-      const result = children.reduce(
-        (carry, element) =>
-          carry +
-          element.querySelector('input[type="number"].app-cmp-input')
-            .valueAsNumber,
-        0
-      );
-  
-      [...componentElement.querySelectorAll("output.app-cmp-result")].forEach(
-        (elem) => {
-          elem.value = result.toLocaleString();
-        }
-      );
-  
-    };
-  
-    const updateList = () => {
-      updateResult();
-  
-      const children = [...inputList.children].filter(
-        (elem) => elem !== tempInput
-      );
-  
-      children.forEach((element, i) => {
-        [...element.querySelectorAll(".app-cmp-input-no")].forEach((elem) => {
-          elem.textContent = `${i + 1}`;
-        });
-      });
+  const tempInput = componentElement.querySelector("template.app-temp-input");
+  const inputList = tempInput.parentElement;
 
-      [...inputList.querySelectorAll(".app-cmd-remove-input")].forEach(
-        (elem) => (elem.disabled = children.length === 1)
-      );
-    };
-  
-    const createElement = () => {
-      const container = tempInput.content.cloneNode(true).firstElementChild;
-  
-      container.addEventListener("click", (ev) => {
-        if (ev.target.matches(".app-cmd-remove-input")) {
-          ev.currentTarget.remove();
-          updateList();
-        }
+  const updateResult = () => {
+    const children = [...inputList.children].filter(
+      (elem) => elem !== tempInput
+    );
+
+    const result = children.reduce(
+      (carry, element) =>
+        carry +
+        element.querySelector('input[type="number"].app-cmp-input')
+          .valueAsNumber,
+      0
+    );
+
+    [...componentElement.querySelectorAll("output.app-cmp-result")].forEach(
+      (elem) => {
+        elem.value = result.toLocaleString();
+      }
+    );
+  };
+
+  const updateList = () => {
+    updateResult();
+
+    const children = [...inputList.children].filter(
+      (elem) => elem !== tempInput
+    );
+
+    children.forEach((element, i) => {
+      [...element.querySelectorAll(".app-cmp-input-no")].forEach((elem) => {
+        elem.textContent = `${i + 1}`;
       });
-  
-      container
-        .querySelector('input[type="number"].app-cmp-input')
-        .addEventListener("change", updateResult);
-  
-      return container;
-    };
-  
-    componentElement.addEventListener("click", (ev) => {
-      if (ev.target.matches(".app-cmp-add-input")) {
-        inputList.append(createElement());
+    });
+
+    [...inputList.querySelectorAll(".app-cmd-remove-input")].forEach(
+      (elem) => (elem.disabled = children.length === 1)
+    );
+  };
+
+  const createElement = () => {
+    const container = tempInput.content.cloneNode(true).firstElementChild;
+
+    container.addEventListener("click", (ev) => {
+      if (ev.target.matches(".app-cmd-remove-input")) {
+        ev.currentTarget.remove();
         updateList();
       }
     });
-  
-    inputList.addEventListener("change", (ev) => {
-      if (ev.target.matches('input[type="number"].app-cmp-input')) {
-        updateResult();
-      }
-    });
-  
-    inputList.append(createElement());
-    updateList();
-  }
-  
-  // npx http-server -c0 ()
 
-  
-  
+    container
+      .querySelector('input[type="number"].app-cmp-input')
+      .addEventListener("change", updateResult);
+
+    return container;
+  };
+
+  componentElement.addEventListener("click", (ev) => {
+    if (ev.target.matches(".app-cmp-add-input")) {
+      inputList.append(createElement());
+      updateList();
+    }
+  });
+
+  inputList.addEventListener("change", (ev) => {
+    if (ev.target.matches('input[type="number"].app-cmp-input')) {
+      updateResult();
+    }
+  });
+
+  inputList.append(createElement());
+  updateList();
+}
+
+//Lab
+window.addEventListener("DOMContentLoaded", function () {
+  const sectionList = document.querySelectorAll("section");
+
+  sectionList.forEach(function (section) {
+    const removeSectionButton = section.querySelector(
+      ".app-cmd-remove-section"
+    );
+    removeSectionButton.addEventListener("click", function () {
+      section.remove();
+    });
+
+    const inputList = section.querySelector(".app-cmp-input-list");
+    const addInputButton = section.querySelector(".app-cmp-add-input");
+    addInputButton.addEventListener("click", function () {
+      const inputTemplate = inputList.querySelector(".app-temp-input");
+      const input = inputTemplate.content.cloneNode(true).firstElementChild;
+
+      const removeInputButton = input.querySelector(".app-cmd-remove-input");
+      removeInputButton.addEventListener("click", function () {
+        input.remove();
+      });
+
+      inputList.appendChild(input);
+    });
+  });
+});
+// npx http-server -c0 ()
