fd0a0c29a210acbd67f4269715c9757627500a59
artAFF <art911191@gmail.com>
2023-07-17 02:44:24 +0700

    origin

--- src/js/dom-06-final.js
+++ src/js/dom-06-final.js
@@ -1,7 +1,5 @@
 import { createComponent } from "./input-component.js";
 
-const componentElement = document.querySelector(".app-cmp-main");
-
 createComponent(document.body);
 
 // npx gh-pages -d src
